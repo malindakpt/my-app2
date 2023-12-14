e39896137ebe0586ec2c67f8900b4d4b89ec7e0d
Malinda Kumarasinghe <malinda.kumarasinghe@openprovider.nl>
2023-12-14 23:20:16 +0530

    Added initial files

---
+++ src/components/topbar/topbar.tsx
@@ -0,0 +1,16 @@
+import './topbar.scss';
+import logo from '../../assets/logo.svg';
+
+export const TopBar = () => <>
+    <div className='topbar'>
+        <div>
+            <img src={logo} />
+        </div>
+
+        <div className='menu-items'>
+            <div>
+                
+            </div>
+        </div>
+    </div>
+</>
