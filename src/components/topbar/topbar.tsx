cac59f795ce4563a5a42dcfefa9b475c01da124e
Malinda Kumarasinghe <malinda.kumarasinghe@openprovider.nl>
2023-12-14 23:43:18 +0530

    Topbar complete

--- src/components/topbar/topbar.tsx
+++ src/components/topbar/topbar.tsx
@@ -3,14 +3,25 @@ import logo from '../../assets/logo.svg';
 
 export const TopBar = () => <>
     <div className='topbar'>
-        <div>
+        <div className='logo'>
             <img src={logo} />
         </div>
 
         <div className='menu-items'>
             <div>
-                
+                SERVICES
+            </div>
+            <div>
+                ABOUT US
+            </div>
+            <div>
+                CONTACT US
+            </div>
+            <div>
+                CAREERS
             </div>
         </div>
+
+        <div className='menu-burger'></div>
     </div>
 </>
