014f1425ad45e6901ae7543197739a2cbe7eccb8
Malinda Kumarasinghe <malinda.kumarasinghe@openprovider.nl>
2023-12-15 08:14:05 +0530

    Fixed Ui issues

---
+++ src/components/middleBanner/middleBanner.tsx
@@ -0,0 +1,11 @@
+import './middleBanner.scss'
+
+export const MiddleBanner = () =>
+    <div className='middle-banner'>
+        We crush your competitors, goals, and sales records - without the B.S.
+
+
+        <div className='button'>
+            Get free consultation
+        </div>
+    </div>;
--- src/components/middleContent/middleContent.tsx
+++ src/components/middleContent/middleContent.tsx
@@ -1,7 +1,8 @@
+import { MiddleBanner } from '../middleBanner/middleBanner';
 import './middleContent.scss';
 
 export const MiddleContent = () => <>
     <div className="middleContent">
-        
+        <MiddleBanner />
     </div>
 </>
